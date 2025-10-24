8849d336fbae042b9b7139a983c4f9bf22200ac6
Alexey Savchenko <alexey.repository@gmail.com>
2025-10-24 14:04:34 +0300

    modify InputOutput entities

--- src/BuildingBlocks.CleanArchitecture.Infrastructure/Persistence/Database/InboxOutbox/InboxMessage.cs
+++ src/BuildingBlocks.CleanArchitecture.Infrastructure/Persistence/Database/InboxOutbox/InboxMessage.cs
@@ -1,13 +1,31 @@
-﻿namespace BuildingBlocks.CleanArchitecture.Infrastructure.Persistence.Database.InboxOutbox;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BuildingBlocks.CleanArchitecture.Infrastructure.Persistence.Database.InboxOutbox;
+
+[Table("inbox_messages")]
 public sealed class InboxMessage
 {
-    public Guid Id { get; set; } 
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("message_id")]
     public Guid MessageId { get; set; }
+
+    [Column("type")]
     public string Type { get; set; } = string.Empty;
+
+    [Column("content")]
     public string Content { get; set; } = string.Empty;
-    public DateTime ReceivedOnUtc { get; set; } 
-    public DateTime? ProcessedOnUtc { get; set; } 
+
+    [Column("received_on_utc")]
+    public DateTime ReceivedOnUtc { get; set; }
+
+    [Column("processed_on_utc")]
+    public DateTime? ProcessedOnUtc { get; set; }
+
+    [Column("error")]
     public string? Error { get; set; }
+
+    [Column("retry_count")]
     public int RetryCount { get; set; }
 }
--- src/BuildingBlocks.CleanArchitecture.Infrastructure/Persistence/Database/InboxOutbox/OutboxMessage.cs
+++ src/BuildingBlocks.CleanArchitecture.Infrastructure/Persistence/Database/InboxOutbox/OutboxMessage.cs
@@ -1,13 +1,31 @@
-﻿namespace BuildingBlocks.CleanArchitecture.Infrastructure.Persistence.Database.InboxOutbox;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BuildingBlocks.CleanArchitecture.Infrastructure.Persistence.Database.InboxOutbox;
+
+[Table("outbox_messages")]
 public sealed class OutboxMessage
 {
+    [Column("id")]
     public Guid Id { get; set; }
+
+    [Column("type")]
     public string Type { get; set; } = string.Empty;
+
+    [Column("message_type")]
     public MessageType MessageType { get; set; } = MessageType.DomainEvent;
+
+    [Column("content")]
     public string Content { get; set; } = string.Empty;
+
+    [Column("occured_on_utc")]
     public DateTime OccuredOnUtc { get; set; }
+
+    [Column("processed_on_utc")]
     public DateTime? ProcessedOnUtc { get; set; }
+
+    [Column("error")]
     public string? Error { get; set; }
+
+    [Column("retry_count")]
     public int RetryCount { get; set; }
 }
